4d6b29456798689ad1e793055ddcb96ab00bf57a
miloradradovic <miloradradovicmico98@gmail.com>
2021-06-03 19:56:10 +0200

    fixed session

--- SBNZ/src/main/java/com/example/SBNZ/service/TrainingService.java
+++ SBNZ/src/main/java/com/example/SBNZ/service/TrainingService.java
@@ -5,18 +5,22 @@ import com.example.SBNZ.model.training.Exercise;
 import com.example.SBNZ.model.training.cep.CEPInput;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.SBNZ.model.training.InputDataTraining;
 import com.example.SBNZ.model.training.Training;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 public class TrainingService {
 
     private final KieContainer kieContainer;
+    private KieSession kieSession;
 
     @Autowired
     private ExerciseService exerciseService;
@@ -28,16 +32,20 @@ public class TrainingService {
 
     public List<Training> getTraining(InputDataTraining input) {
         List<Exercise> exercises = exerciseService.findAll();
-        KieSession kieSession = kieContainer.newKieSession("trainingSession");
-        // CurrentFact currentFact = new CurrentFact(2);
+        if (kieSession == null) {
+            kieSession = kieContainer.newKieSession("trainingSession");
+        }
         kieSession.insert(input);
         for (Exercise exercise : exercises) {
             kieSession.insert(exercise);
         }
-        // kieSession.insert(currentFact);
         kieSession.getAgenda().getAgendaGroup("Ruleflow1").setFocus();
         kieSession.fireAllRules();
-        kieSession.dispose();
+        Collection<FactHandle> handlers = kieSession.getFactHandles();
+        for (FactHandle handle: handlers) {
+            kieSession.delete(handle);
+        }
+        // kieSession.dispose(); dispose radi samo kad se korisnik izloguje
         return input.getTraining();
     }
 
